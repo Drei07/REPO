97e2c9735d0ef02bd83bac39c2a34eca77dd3a61
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2022-11-15 12:36:45 +0800

    MAIN

--- initialize.php
+++ initialize.php
@@ -9,7 +9,7 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 // if(!defined('DB_NAME')) define('DB_NAME',"repo_db");
 
 
-    if(!defined('base_url')) define('base_url','https://dhvsu.website/');
+    if(!defined('base_url')) define('base_url','https://dhvsurepo.website/');
     if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
     if(!defined('dev_data')) define('dev_data',$dev_data);
     if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
